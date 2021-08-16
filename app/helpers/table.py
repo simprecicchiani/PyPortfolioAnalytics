6b39d6e1a44e29c94733611d6f7ee432c644aab8
Simone Precicchiani <simone.precicchiani@gmail.com>
2021-08-16 03:05:07 +0200

    replace and improve table class

---
+++ app/helpers/table.py
@@ -0,0 +1,98 @@
+import pandas as pd
+from numpy import genfromtxt
+import base64
+from os import getenv
+import datetime
+
+class Orders:
+    
+    ACCOUNT = ['deposit', 'withdrawal']
+    SECURITY = ['purchase', 'sale']
+
+class Tickers:
+
+    US = genfromtxt(getenv('PWD')+'/assets/us_tickers.csv', dtype=str, delimiter=',')
+    CURRENCIES = genfromtxt(getenv('PWD')+'/assets/currencies.csv', dtype=str, delimiter=',')
+
+class Table:
+
+    def new(self):
+
+        COLS = ['Date','Ticker', 'Order', 'Price', 'Quantity', 'Fee']
+        self.data = pd.DataFrame(columns=COLS)
+
+    def __init__(self):
+        self.new()
+
+    @staticmethod
+    def validate(date, ticker, order, price, quantity, fee):
+
+        ticker = ticker.upper()
+        ACCOUNT_TRANSACTION = (True if ticker == 'CASH.' + getenv('ACCOUNT_CURRENCY') else False)
+        ORDERS = (Orders.ACCOUNT if ACCOUNT_TRANSACTION else Orders.SECURITY)
+
+        if not isinstance(date, datetime.date):
+            try: 
+                date = datetime.datetime.strptime(date, '%Y-%m-%d')
+            except ValueError:
+                return print(f'{date} does not match format %Y-%m-%d (1970-01-01)'), None
+
+        if not isinstance(ticker, str):
+            # raise TypeError('Ticker is mandatory and should be a string.')
+            return print('Ticker is mandatory and should be a string.'), None
+        elif not ACCOUNT_TRANSACTION and not (ticker in Tickers.US):
+            # raise Exception(f'{ticker} is not available.')
+            return print(f'{ticker} is not available.'), None
+
+        if not isinstance(order, str):
+            # raise TypeError('Order is mandatory and should be a string.')
+            return print('Order is mandatory and should be a string.'), None
+        elif not (order in ORDERS):
+            # raise Exception(f'{order} is not a valid order. Valid orders are {ORDERS}')
+            return print(f'{order} is not a valid order. Valid orders are {ORDERS}'), None
+
+        if not isinstance(price, (int, float)):
+            # raise TypeError('Price is mandatory and should be a number.')
+            return print('Price is mandatory and should be a number.'), None
+
+        if not isinstance(quantity, (int, float)):
+            # raise TypeError('Quantity is mandatory and should be a number.')
+            return print('Quantity is mandatory and should be a number.'), None
+
+        if not isinstance(fee, (int, float)):
+            # raise TypeError('Fee should be a number.')
+            return print('Fee should be a number.'), None
+
+        TRANSACTION = {
+            'Date' : date,
+            'Ticker' : ticker,
+            'Order' : order,
+            'Price' : price,
+            'Quantity' : quantity,
+            'Fee' : fee
+        }
+
+        return True, TRANSACTION
+
+    def add(self, date, ticker, order, price, quantity, fee):
+
+        check, TRANSACTION = self.validate(date, ticker, order, price, quantity, fee)
+
+        if check:
+            self.data = self.data.append(TRANSACTION, ignore_index=True)
+        else:
+            check
+
+    def add_from_csv(self, csv):
+
+        csv = pd.read_csv(csv)
+
+        for i, row in csv.iterrows():
+            self.add(row.Date, row.Ticker, row.Order, row.Price, row.Quantity, row.Fee)
+
+    def delete(self, index):
+        self.data = self.data.drop([index]).reset_index(drop=True)
+
+    def download(self):
+        b64 = base64.b64encode(self.data.to_csv(index=False).encode()).decode()
+        return f'<a href="data:file/txt;base64,{b64}" download="Portfolio.csv">Download as CSV</a>'
---
+++ app/pages/builder.py
@@ -0,0 +1,105 @@
+from posix import environ
+import streamlit as st
+import pandas as pd
+from os import environ
+from helpers.table import Orders, Tickers, Table
+from helpers.portfolio import Portfolio
+from helpers.alphavantage import av
+
+# PAGE SETUP
+
+def app():
+
+    if 'table' not in st.session_state:
+        st.session_state.table = Table()
+
+    # VARIABLES (updated on every session)
+
+    LEN = len(st.session_state.table.data)
+    EMPTY = (True if LEN == 0 else False)
+
+    
+    with st.expander('Options'):
+
+        col01, col02 = st.columns(2)
+
+        with col01:
+            environ['ACCOUNT_CURRENCY'] = st.selectbox('Portfolio Currency', Tickers.CURRENCIES, index=144)
+        with col02:
+            environ['ALPHAVANTAGE_API_KEY'] = st.text_input('Alpha Vantage API')
+            st.write('[Get a free api key](https://www.alphavantage.co/support/#api-key)')
+
+    if environ.get('ALPHAVANTAGE_API_KEY') != '':
+        with st.expander('Upload'):
+        
+            uploaded_file = st.file_uploader('', type=['.csv'])
+
+            if uploaded_file != None:
+                st.button('Load on memory', on_click=st.session_state.table.add_from_csv, args=(uploaded_file,))
+
+
+        with st.expander('Add manually'):
+
+            col11, col12 = st.columns([2,3])
+            col21, col22 = st.columns(2)
+            col31, col32, col33 = st.columns([3,3,2])
+
+            with col11:
+                transaction_date = st.date_input('Transaction Date')
+            with col12:
+                transaction_type = st.selectbox('Transaction type', ['Account','Security'])
+                SECURITY = (True if transaction_type == 'Security' else False)
+                
+            if SECURITY:
+                with col21:
+                    transaction_ticker = st.selectbox('Ticker', Tickers.US)
+                with col22:
+                    transaction_price = st.number_input('Price', 0.0, None, 0.0, 10.0)
+                with col31:
+                    transaction_order = st.selectbox('Order', Orders.SECURITY)
+            
+            else:
+                transaction_ticker = 'CASH.USD' # account currency
+                transaction_price = 1
+                with col31:
+                    transaction_order = st.selectbox('Order', Orders.ACCOUNT)
+
+            with col32:
+                transaction_quantity = st.number_input('Quantity', 0.0, None, 0.0, 10.0)
+            with col33:
+                transaction_fee = st.number_input('Fee', 0.0, None, 0.0, 10.0)
+
+            st.button('Add', on_click=st.session_state.table.add, args=(
+                transaction_date,
+                transaction_ticker,
+                transaction_order,
+                transaction_price,
+                transaction_quantity,
+                transaction_fee
+            ,))
+
+
+        if not EMPTY:
+
+            with st.expander('Edit'):
+
+                col41, col42= st.columns([1,2])
+
+                with col41:
+                    st.button('Clear Table', on_click=st.session_state.table.new)
+
+                if LEN > 1:
+                    with col42:
+                        index = st.slider('Select row number', 0, LEN-1,0,1)
+                        st.button('Delete row', on_click=st.session_state.table.delete, args=(index,))
+
+            st.subheader('')
+            st.write(st.session_state.table.data)
+            st.markdown(st.session_state.table.download(), unsafe_allow_html=True)
+
+            def run_logic(df):
+                df = df.set_index('Date').sort_index()
+                st.session_state.portfolio = Portfolio(df)
+                st.session_state.portfolio.run()
+
+            st.button('Run App', on_click=run_logic, args=(st.session_state.table.data,))
